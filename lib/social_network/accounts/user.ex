bff4251625ffac821890bbd4e65ef665d92cf2a7
PavelBe <worldisaduck@gmail.com>
2019-08-01 21:28:43 +0300

    Added virtual fields to user

--- lib/social_network/accounts/user.ex
+++ lib/social_network/accounts/user.ex
@@ -6,15 +6,30 @@ defmodule SocialNetwork.Accounts.User do
     field :encrypted_password, :string
     field :username, :string
 
+		field :password, :string, virtual: true
+		field :password_confirmation, :string, virtual: true
+
     timestamps()
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :encrypted_password])
-    |> validate_required([:username, :encrypted_password])
+    |> cast(attrs, [:username, :password, :password_confirmation])
+    |> validate_required([:username, :password, :password_confirmation])
+		|> validate_length(:password, min: 6)
+		|> validate_confirmation(:password)
     |> unique_constraint(:username)
-		|> update_change(:encrypted_password, &Bcrypt.hash_pwd_salt/1)
+		|> hash_password
   end
+
+	defp hash_password(changeset) do
+		case changeset do
+			%Ecto.Changeset{valid?: true, changes: %{password: pass}}
+				->
+					put_change(changeset, :encrypted_password, Bcrypt.hash_pwd_salt(pass))
+			_ ->
+				changeset
+		end
+	end
 end
