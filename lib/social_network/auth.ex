6e1bbaccf51003c13a1d513c7b111b6ff0f44ce5
PavelBe <worldisaduck@gmail.com>
2019-08-06 17:12:21 +0300

    Improved authentication

--- lib/social_network/auth.ex
+++ lib/social_network/auth.ex
@@ -1,12 +1,16 @@
 defmodule SocialNetwork.Auth do
 	def encode_and_sign(user, params \\ %{}) do
-		secret = Application.get_env(:social_network, SocialNetworkWeb.Endpoint)[:secret_key_base]
 		encoded_header = header |> Jason.encode! |> Base.url_encode64
 		encoded_payload = user |> payload |> Jason.encode! |> Base.url_encode64
-		encoded_signature = :crypto.hmac(:sha256, secret, "#{encoded_header}.#{encoded_payload}") |> Base.url_encode64
+		encoded_signature = signature(encoded_header, encoded_payload) |> Base.url_encode64
 
 		token = [encoded_header, encoded_payload, encoded_signature] |> Enum.join(".")
 		{:ok, token}
+	end
+
+	def signature(encoded_header, encoded_payload) do
+		secret = Application.get_env(:social_network, SocialNetworkWeb.Endpoint)[:secret_key_base]
+		:crypto.hmac(:sha256, secret, "#{encoded_header}.#{encoded_payload}")		
 	end
 
 	defp header do
--- lib/social_network_web/plugs/auth/authenticate_token.ex
+++ lib/social_network_web/plugs/auth/authenticate_token.ex
@@ -1,28 +1,40 @@
 defmodule SocialNetworkWeb.Plugs.Auth.AuthenticateToken do
 	import Plug.Conn
+	alias SocialNetwork.Auth
 	require IEx
+
 	def init(opts), do: opts
 
 	def call(conn, _opts) do
 		with {:ok, token} <- fetch_token_from_header(conn),
-				 {:ok, token} <- verify_claims(token) do
+				 {:ok, token} <- verify_signature(token) do
+			conn
 		else
 			:no_token_found ->
 				send_resp(conn, 401, "no token found")	
 			{:error, :invalid_token} ->
-				IEx.pry
+				send_resp(conn, 401, "invalid token")	
 		end
 	end
 
 	defp fetch_token_from_header(conn) do
 		case get_req_header(conn, "authorization") do
-   		["Bearer " <> token] -> token
+   		["Bearer " <> token] -> {:ok, token}
     	[anything] -> :wrong_authorization_type
     	_	-> :no_token_found
   	end
 	end
 
-	defp verify_claims(token) do
+	defp verify_signature(token) do
+		[encoded_header, encoded_payload, encoded_signature] = fetch_token_parts(token)	
+		IEx.pry
+		case Base.url_decode64!(encoded_signature) == Auth.signature(encoded_header, encoded_payload) do
+			true 	-> {:ok, token} 
+			false -> {:error, :invalid_token}
+		end
+	end
 
+	defp fetch_token_parts(token) do
+		token |> String.split(".")
 	end
 end
