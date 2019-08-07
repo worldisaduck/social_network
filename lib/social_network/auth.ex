06fa74919d7161b0aca663e51ea7c4f9b655227d
PavelBe <worldisaduck@gmail.com>
2019-08-07 15:10:29 +0300

    Moved auth logic to Auth module

--- lib/social_network/auth.ex
+++ lib/social_network/auth.ex
@@ -8,6 +8,18 @@ defmodule SocialNetwork.Auth do
 		{:ok, token}
 	end
 
+	def verify_token(token) do
+		splited_token = String.split(token, ".")
+		[encoded_header, encoded_payload, encoded_signature] = splited_token
+		with :ok <- decode_and_verify_signature(splited_token),
+				 :ok <- decode_and_verify_claims(encoded_payload) do
+			{:ok, token}
+		else
+			{:error, :invalid_token} ->
+				{:error, :invalid_token}
+		end
+	end
+
 	def signature(encoded_header, encoded_payload) do
 		secret = Application.get_env(:social_network, SocialNetworkWeb.Endpoint)[:secret_key_base]
 		:crypto.hmac(:sha256, secret, "#{encoded_header}.#{encoded_payload}")		
@@ -27,4 +39,25 @@ defmodule SocialNetwork.Auth do
 			"exp" => DateTime.to_unix(DateTime.utc_now) + 365 * 24 * 60 * 60
 		}
 	end
+
+	defp decode_and_verify_signature(splited_token) do
+		[encoded_header, encoded_payload, encoded_signature] = splited_token
+		case Base.url_decode64!(encoded_signature, padding: true) == signature(encoded_header, encoded_payload) do
+			true 	-> :ok
+			false -> {:error, :invalid_token}
+		end
+	end
+
+	defp decode_and_verify_claims(claims) do
+		case claims |> Base.url_decode64!(padding: false) |> Jason.decode! |> Map.get("exp") do
+			nil -> :ok
+			exp_at_in_sec ->
+				if exp_at_in_sec > DateTime.to_unix(DateTime.utc_now) do
+					:ok
+				else
+					{:error, :invalid_token}
+				end
+		end
+	end
 end
+
--- lib/social_network_web/plugs/auth/authenticate_token.ex
+++ lib/social_network_web/plugs/auth/authenticate_token.ex
@@ -5,11 +5,10 @@ defmodule SocialNetworkWeb.Plugs.Auth.AuthenticateToken do
 
 	def init(opts), do: opts
 
-	def call(%Plug.Conn{path_info: ["api", "v1", "sign-up"]} = conn, _opts), do: conn
 
 	def call(conn, _opts) do
 		with {:ok, token} <- fetch_token_from_header(conn),
-				 {:ok, token} <- verify_token(token) do
+				 {:ok, token} <- Auth.verify_token(token) do
 			conn
 		else
 			:no_token_found ->
@@ -25,37 +24,5 @@ defmodule SocialNetworkWeb.Plugs.Auth.AuthenticateToken do
     	[anything] -> :wrong_authorization_type
     	_	-> :no_token_found
   	end
-	end
-
-	defp verify_token(token) do
-		splited_token = String.split(token, ".")
-		[encoded_header, encoded_payload, encoded_signature] = splited_token
-		with :ok <- decode_and_verify_signature(splited_token),
-				 :ok <- decode_and_verify_claims(encoded_payload) do
-			{:ok, token}
-		else
-			{:error, :invalid_token} ->
-				{:error, :invalid_token}
-		end
-	end
-
-	defp decode_and_verify_signature(splited_token) do
-		[encoded_header, encoded_payload, encoded_signature] = splited_token
-		case Base.url_decode64!(encoded_signature, padding: true) == Auth.signature(encoded_header, encoded_payload) do
-			true 	-> :ok
-			false -> {:error, :invalid_token}
-		end
-	end
-
-	defp decode_and_verify_claims(claims) do
-		case claims |> Base.url_decode64!(padding: false) |> Jason.decode! |> Map.get("exp") do
-			nil -> :ok
-			exp_at_in_sec ->
-				if exp_at_in_sec > DateTime.to_unix(DateTime.utc_now) do
-					:ok
-				else
-					{:error, :invalid_token}
-				end
-		end
 	end
 end
