9ebb46633020c584abfafa00cc4696e2bc648fad
PavelBe <worldisaduck@gmail.com>
2019-08-17 02:46:53 +0300

    Test changes

--- lib/social_network/accounts/user.ex
+++ lib/social_network/accounts/user.ex
@@ -1,33 +1,32 @@
 defmodule SocialNetwork.Accounts.User do
-  use Ecto.Schema
-  import Ecto.Changeset
+	use Ecto.Schema
+	import Ecto.Changeset
 
-  schema "users" do
-    field :encrypted_password, :string
-    field :username, :string
+	schema "users" do
+		field :encrypted_password, :string
+		field :username, :string
 
 		field :password, :string, virtual: true
 		field :password_confirmation, :string, virtual: true
 
-    timestamps()
-  end
+		timestamps()
+	end
 
-  @doc false
-  def changeset(user, attrs) do
-    user
-    |> cast(attrs, [:username, :password, :password_confirmation])
-    |> validate_required([:username, :password, :password_confirmation])
+	@doc false
+	def changeset(user, attrs) do
+		user
+		|> cast(attrs, [:username, :password, :password_confirmation])
+		|> validate_required([:username, :password, :password_confirmation])
 		|> validate_length(:password, min: 6)
 		|> validate_confirmation(:password)
-    |> unique_constraint(:username)
+		|> unique_constraint(:username)
 		|> hash_password
-  end
+	end
 
 	defp hash_password(changeset) do
 		case changeset do
-			%Ecto.Changeset{valid?: true, changes: %{password: pass}}
-				->
-					put_change(changeset, :encrypted_password, Bcrypt.hash_pwd_salt(pass))
+			%Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+				put_change(changeset, :encrypted_password, Bcrypt.hash_pwd_salt(pass))
 			_ ->
 				changeset
 		end
