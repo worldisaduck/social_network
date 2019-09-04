0de840e5b0c36e21c47bcaf27388b21c2e1ca846
PavelBe <worldisaduck@gmail.com>
2019-09-04 03:19:35 +0300

    Fixed indent

--- lib/social_network_web/router.ex
+++ lib/social_network_web/router.ex
@@ -10,24 +10,24 @@ defmodule SocialNetworkWeb.Router do
   end
 
   pipeline :api do
-		plug CORSPlug, origin: "*"
+    plug CORSPlug, origin: "*"
     plug :accepts, ["json"]
-		plug :fetch_session
+    plug :fetch_session
   end
 
-	pipeline :jwt_auth do
-		plug SocialNetworkWeb.Plugs.Auth.AuthenticateToken 
-	end
+  pipeline :jwt_auth do
+    plug SocialNetworkWeb.Plugs.Auth.AuthenticateToken 
+  end
 
 
   scope "/" do
     pipe_through :jwt_auth
 
-	  forward "/graphiql", Absinthe.Plug.GraphiQL, schema: SocialNetwork.Schema
+    forward "/graphiql", Absinthe.Plug.GraphiQL, schema: SocialNetwork.Schema
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", SocialNetworkWeb do
-  #   pipe_through :api
-  # end
+# Other scopes may use custom stacks.
+# scope "/api", SocialNetworkWeb do
+#   pipe_through :api
+# end
 end
